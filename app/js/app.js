69f0459e9399d51f4e768c8c781ff13c44b0ae57
Jaime Velaz <jaume.velaz@gmail.com>
2014-04-04 20:44:59 +0200

    Estructura Inicial

---
+++ app/js/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.filters',
+  'myApp.services',
+  'myApp.directives',
+  'myApp.controllers',
+  'ui.bootstrap'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/about', {templateUrl: 'partials/about.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/contact', {templateUrl: 'partials/contact.html', controller: 'MyCtrl3'});
+  $routeProvider.otherwise({redirectTo: '/home'});
+}]);
---
+++ app/js/controllers.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('myApp.controllers', [])
+  .controller('MyCtrl1', [function() {
+
+  }])
+  .controller('MyCtrl2', [function() {
+
+  }])
+  .controller('MyCtrl3', [function() {
+
+  }]);
